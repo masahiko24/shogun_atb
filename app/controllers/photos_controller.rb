f076ee6e33d7999bfcb5ac1a01c98e3111f8d8bd
masahiko24 <104701547+masahiko24@users.noreply.github.com>
2022-06-18 17:26:45 +0900

    ルーティングの設定(photo)

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,2 +1,6 @@
 class PhotosController < ApplicationController
-end
+
+  def index
+  end
+
+  end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
+  root to: 'photos#index'
+  resources :photos
 end
+
