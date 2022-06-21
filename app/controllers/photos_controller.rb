ae50fbfe6b6caa30a74e966372ccf63f6bfd15b9
masahiko24 <104701547+masahiko24@users.noreply.github.com>
2022-06-21 12:36:58 +0900

    PhotosController UpDate

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,7 +1,19 @@
 class PhotosController < ApplicationController
-
   def index
     @photo = Photo.all
   end
 
+  def new
+    @photo = Photo.new
   end
+
+  def create
+    @photo = Photo.new(photo_params)
+  end
+
+  private
+
+  def photo_params
+    params.require(:photo).permit(:title, :image, :memo).merge(user_id: current_user.id)
+  end
+end
