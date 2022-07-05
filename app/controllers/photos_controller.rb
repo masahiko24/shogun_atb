979722ee55a007cf7fab1054cb11cb6205ca23bb
masahiko24 <104701547+masahiko24@users.noreply.github.com>
2022-07-05 22:15:54 +0900

    コントローラー追加

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,5 +1,7 @@
 class PhotosController < ApplicationController
-  before_action :set_photo, only: [:edit, :show, :destroy, :update]
+  # before_action :set_photo, only: [:edit, :show, :destroy, :update]
+  before_action :authenticate_user!, except: [:index, :show]
+
 
   def index
     @photo = Photo.all.order(created_at: :desc)
@@ -44,9 +46,9 @@ class PhotosController < ApplicationController
     params.require(:photo).permit(:nickname, :title, :image, :memo).merge(user_id: current_user.id)
   end
 
-  def set_photo
-    @photo = Photo.find(params[:id])
-  end
+  # def set_photo
+  #   @photo = Photo.find(params[:id])
+  # end
 
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,18 @@
 class UsersController < ApplicationController
+  
+  
   def show
     @user = User.find(params[:id])
     @photos = @user.photos
+  end
+
+  private
+
+  def photo_params
+    params.require(:photo).permit(:nickname, :title, :image, :memo).merge(user_id: current_user.id)
+  end
+
+  def set_item
+    @user = User.find(params[:id])
   end
 end
