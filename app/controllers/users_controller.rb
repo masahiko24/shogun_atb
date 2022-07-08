96f539709ca80b9b344f576d9cf115dcd3470cb3
masahiko24 <104701547+masahiko24@users.noreply.github.com>
2022-07-08 12:47:49 +0900

    インデント修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,7 @@
 class UsersController < ApplicationController
-  
-  
   def show
     @user = User.find(params[:id])
     @photo = Photo.find(params[:id])
     @photos = @user.photos.includes(:user).order(created_at: :desc)
-
-
   end
-
 end
