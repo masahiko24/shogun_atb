57e80aa36ceba55f87244046ad4f265222904692
masahiko24 <104701547+masahiko24@users.noreply.github.com>
2022-07-17 20:20:47 +0900

    Update users_controller.rb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,6 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
-    @photo = Photo.find(params[:id])
     @photos = @user.photos.includes(:user).order(created_at: :desc)
   end
 end
