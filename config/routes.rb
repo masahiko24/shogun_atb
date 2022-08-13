09f47f16d0b2df464e75e3f2456ba9a99f49961a
masahiko24 <104701547+masahiko24@users.noreply.github.com>
2022-08-13 18:16:01 +0900

    ルーティング修正

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,10 @@ Rails.application.routes.draw do
   root to: 'photos#index'
   resources :photos do
     resources :comments, only: [:create, :destroy]
+    resources :favarites, only: [:create, :destroy]
   end
 
-  resources :users
+  resources :users, only: [:index, :show]
 
 end
 
