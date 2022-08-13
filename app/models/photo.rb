35ffd825578781cbf8ed52e90b5fd6568fd281c0
masahiko24 <104701547+masahiko24@users.noreply.github.com>
2022-08-13 18:20:05 +0900

    モデル修正

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,12 +1,9 @@
 class Photo < ApplicationRecord
   belongs_to :user
   has_many :comments, dependent: :destroy
-<<<<<<< Updated upstream
-=======
   has_many :favarites, dependent: :destroy
   has_many :favarited_users, through: :favarites, source: :user
 
->>>>>>> Stashed changes
   has_one_attached :image
 
   validates :nickname, presence: true
