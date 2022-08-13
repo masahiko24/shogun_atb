8ddbb5112f2897fa2b3c5e9c7b3f48354a25cb6f
masahiko24 <104701547+masahiko24@users.noreply.github.com>
2022-08-13 17:29:51 +0900

    インデント修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,8 +6,20 @@ class User < ApplicationRecord
 
   has_many :photos, dependent: :destroy
   has_many :comments, dependent: :destroy
+<<<<<<< Updated upstream
+=======
+  has_many :favarites, dependent: :destroy
+  has_many :favarited_photos, through: :favarites, source: :photo
+>>>>>>> Stashed changes
 
   validates :name, presence: true
   validates :email, presence: true
   validates :password, presence: true
+<<<<<<< Updated upstream
+=======
+
+  def already_favarited?(photo)
+    favarites.exists?(photo_id: photo.id)
+  end
+>>>>>>> Stashed changes
 end
