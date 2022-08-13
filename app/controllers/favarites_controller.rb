e63cc6b3b408322b75c40e599399f0226286e795
masahiko24 <104701547+masahiko24@users.noreply.github.com>
2022-08-13 18:19:42 +0900

    コントローラー修正

---
+++ app/controllers/favarites_controller.rb
@@ -0,0 +1,12 @@
+class FavaritesController < ApplicationController
+  def create
+    @favarite = current_user.favarites.create(photo_id: params[:photo_id])
+    redirect_back(fallback_location: root_path)
+  end
+
+  def destroy
+    @favarite = Favarite.find_by(photo_id: params[:photo_id], user_id: current_user.id)
+    @favarite.destroy
+    redirect_back(fallback_location: root_path)
+  end
+end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,8 @@
 class UsersController < ApplicationController
+  def index
+    @users = User.all
+  end
+
   def show
     @user = User.find(params[:id])
     @photos = @user.photos.includes(:user).order(created_at: :desc)
